cb9696532aa7bfc171080c6bca2d7b1359858ded
unknown <thaisamorandini@gmail.com>
2021-04-01 14:56:48 -0300

    Created initial find all  clients GET before find paged

---
+++ backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dsclient.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsclient.dto.ClientDTO;
+import com.devsuperior.dsclient.entities.Client;
+import com.devsuperior.dsclient.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+	
+	@Autowired
+	private ClientRepository repository;
+	
+	/*Por enquanto esta listando todos os clientes*/
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll(){
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	}
+	
+}
