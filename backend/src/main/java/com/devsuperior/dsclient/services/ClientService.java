8f7694e0d60d153e3dcc652d0b2dc0be9bce22de
unknown <thaisamorandini@gmail.com>
2021-04-01 17:45:46 -0300

    Created method PUT

--- backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,30 +17,44 @@ import com.devsuperior.dsclient.services.exceptions.ResourceEntityNotFoundExcept
 
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository repository;
-	
-	/*Por enquanto esta listando todos os clientes*/
+
+	/* Por enquanto esta listando todos os clientes */
 	@Transactional(readOnly = true)
-	public List<ClientDTO> findAll(){
+	public List<ClientDTO> findAll() {
 		List<Client> list = repository.findAll();
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
-	
+
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
 		Client entity = obj.orElseThrow(() -> new ResourceEntityNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
 	}
-	
+
 	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
 		Client entity = new Client();
 		copyEntityToDto(entity, dto);
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyEntityToDto(entity, dto);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			// TODO: handle exception
+			throw new ResourceEntityNotFoundException("Id not found " + id);
+		}
+
 	}
 
 	private void copyEntityToDto(Client entity, ClientDTO dto) {
@@ -49,9 +65,5 @@ public class ClientService {
 		entity.setBirthDate(dto.getBirthDate());
 		entity.setChildren(dto.getChildren());
 	}
-	
-	
-	
-	
-	
+
 }
